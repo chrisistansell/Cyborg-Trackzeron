5bca6b7cb60c99acaf037ecd86d9cd9130f29a26
Chris <74263400+chrisistansell@users.noreply.github.com>
2024-02-25 04:04:09 -0500

    Add files via upload

--- QMK/keymaps/via/keymap.c
+++ QMK/keymaps/via/keymap.c
@@ -20,16 +20,11 @@
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT( 
-      
-    [0] = LAYOUT( /* Base */
-//         index    middle    ring       pinky         
-           KC_WH_U, KC_PGUP,  KC_EQL,    KC_PAST, _______, _______,
-           KC_WH_D, KC_PGDN,  KC_MINS,   KC_PSLS, _______, _______,
-  KC_KP_6, KC_BTN1, KC_BTN2,  KC_BTN3,   KC_BTN4,
-  KC_KP_1, KC_KP_2, KC_KP_3,  KC_KP_4,   KC_KP_5, 
-  KC_TAB,  KC_KP_7, KC_KP_8,  KC_KP_9,   KC_KP_0
-//^ Pinky, Index, Thumb
+    [0] = LAYOUT( KC_PAUSE, KC_TAB,
+        KC_KP_MINUS, KC_KP_ASTERISK, KC_KP_SLASH, KC_KP_0,
+        KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_PLUS,
+KC_KP_4, KC_BTN1, KC_BTN2, KC_KP_5, KC_KP_6,
+ 	    KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_DOT 
  	    ),
     [1] = LAYOUT( _______, _______,           
       _______, _______, _______, _______, 
