aec80a2b3495dc877e9d9bb2b309119468c1b20d
Chris <74263400+chrisistansell@users.noreply.github.com>
2024-02-25 04:01:32 -0500

    Add files via upload

--- QMK for Cyborg/trackzeron/keymaps/via/keymap.c
+++ QMK for Cyborg/trackzeron/keymaps/via/keymap.c
@@ -22,13 +22,14 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT( 
       
-  //s.index  index     middle        ring           pinky 
-           KC_KP_8,   KC_KP_9,   
-           KC_KP_7,   KC_KP_6,      KC_KP_5,       KC_KP_4,
-           KC_WH_U    KC_BTN3,      KC_KP_3,       KC_KP_2,
-  KC_TAB,  KC_BTN1,   KC_BTN2,      KC_LEFT_SHIFT, KC_KP_1,   
-  KC_SPC,  KC_WH_D,   KC_LEFT_ALT,  KC_ESC,        KC_KP_0
-//Thumb^ 
+    [0] = LAYOUT( /* Base */
+//         index    middle    ring       pinky         
+           KC_WH_U, KC_PGUP,  KC_EQL,    KC_PAST, _______, _______,
+           KC_WH_D, KC_PGDN,  KC_MINS,   KC_PSLS, _______, _______,
+  KC_KP_6, KC_BTN1, KC_BTN2,  KC_BTN3,   KC_BTN4,
+  KC_KP_1, KC_KP_2, KC_KP_3,  KC_KP_4,   KC_KP_5, 
+  KC_TAB,  KC_KP_7, KC_KP_8,  KC_KP_9,   KC_KP_0
+//^ Pinky, Index, Thumb
  	    ),
     [1] = LAYOUT( _______, _______,           
       _______, _______, _______, _______, 
