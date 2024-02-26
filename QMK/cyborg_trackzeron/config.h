4e38837829fabc9b65fe23e62db2a066c50613d5
Chris <74263400+chrisistansell@users.noreply.github.com>
2024-02-25 23:01:34 -0500

    Add files via upload

---
+++ QMK/cyborg_trackzeron/config.h
@@ -0,0 +1,30 @@
+/* Copyright 2020 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
+ * Copyright 2019 Sunjun Kim
+ * Copyright 2020 Ploopy Corporation
+ * Copyright 2020 MK
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+// #define ROTATIONAL_TRANSFORM_ANGLE 0
+#define POINTING_DEVICE_INVERT_Y
+
+/* PMW3360 Settings */
+#define POINTING_DEVICE_CS_PIN B0
+
+/* Much more so than a keyboard, speed matters for a mouse. So we'll go for as high
+   a polling rate as possible. */
+#define USB_POLLING_INTERVAL_MS 1
+#define USB_MAX_POWER_CONSUMPTION 100
