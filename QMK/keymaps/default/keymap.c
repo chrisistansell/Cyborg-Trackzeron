c7c8f336117ece41b1b2903199e09d377171909e
Chris <74263400+chrisistansell@users.noreply.github.com>
2024-03-04 16:33:37 -0500

    Profiles were broken....
    Fixed DIP-switch-map section.  Implemented its control into the LED & Profiles.

---
+++ QMK/keymaps/default/keymap.c
@@ -0,0 +1,122 @@
+/* Copyright 2020 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
+ * Copyright 2019 Sunjun Kim
+ * Copyright 2020 Ploopy Corporation
+ * Copyright 2020 MK
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+#include "config.h"  // Include your configuration file
+#include "quantum.h"
+
+// Suspend Settings
+bool suspended = false; // Keep track of the system's suspend state
+
+// Layer names
+#define PROFILE_1             0
+#define SECOND_LAYER_PROFILE_1 1
+#define PROFILE_2             2
+#define SECOND_LAYER_PROFILE_2 3
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {         
+    [PROFILE_1] = {
+        //          index    middle    ring       pinky         
+        {_______, KC_WH_U, KC_PGUP, KC_EQL,  KC_PAST, _______, _______},
+        {_______, KC_WH_D, KC_PGDN, KC_MINS, KC_PSLS, _______, _______},
+        {_______, KC_BTN1, KC_BTN2, KC_BTN3, KC_BTN4, _______, _______},
+        {KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_4, KC_KP_5, KC_KP_6, _______},
+        {KC_TAB,  KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_0, _______, MO(SECOND_LAYER_PROFILE_1)}
+        //^ Pinky, Index, Thumb
+    },
+
+    [SECOND_LAYER_PROFILE_1] = {
+        // Define the keymap for the second layer of profile 1 here
+        {_______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , _______, _______},
+        {_______, KC_F5  , KC_F6  , KC_F7  , KC_F8  , _______, _______},
+        {_______, KC_BTN1, KC_BTN2, KC_BTN3, KC_BTN4, _______, _______},
+        {_______, _______, _______, _______, _______, _______, _______},
+        {_______, _______, _______, _______, _______, _______, _______}
+        //^ Pinky, Index, Thumb
+    },
+
+    [PROFILE_2] = {
+        //          index    middle    ring       pinky         
+        {_______, KC_HOME, KC_PGUP, KC_EQL,  KC_PAST, _______, _______},
+        {_______, KC_END , KC_PGDN, KC_MINS, KC_PSLS, _______, _______},
+        {_______, KC_BTN1, KC_BTN2, KC_BTN3, KC_BTN4, _______, _______},
+        {KC_KP_6, KC_KP_5, KC_KP_4, KC_KP_3, KC_KP_2, KC_KP_1, _______},
+        {KC_TAB,  KC_KP_0, KC_KP_9, KC_KP_8, KC_KP_7, _______, MO(SECOND_LAYER_PROFILE_2)}
+        //^ Pinky, Index, Thumb
+    },
+
+    [SECOND_LAYER_PROFILE_2] = {
+        // Define the keymap for the second layer of profile 2 here
+        {_______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , _______, _______},
+        {_______, KC_F5  , KC_F6  , KC_F7  , KC_F8  , _______, _______},
+        {_______, KC_BTN1, KC_BTN2, KC_BTN3, KC_BTN4, _______, _______},
+        {_______, _______, _______, _______, _______, _______, _______},
+        {_______, _______, _______, _______, _______, _______, _______}
+        //^ You can customize this keymap for the second layer of profile 2
+    },
+};
+
+void suspend_wakeup_task(void) {
+    if (suspended) {
+        // Code to wake up from suspend
+        // This can include sending a specific keypress or using a wake-up API
+        // For example, you might send a key press like this:
+        register_code(KC_SYSTEM_WAKE); // Replace KC_SYSTEM_WAKE with the appropriate keycode for waking up the system
+        unregister_code(KC_SYSTEM_WAKE);
+
+        suspended = false; // Update the state
+    }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case KC_BTN1: // Example: Left mouse button
+            if (record->event.pressed) {
+                suspend_wakeup_task();
+            }
+            break;
+        // Add cases for other keys or mouse buttons if needed
+    }
+    return true;
+}
+
+bool dip_switch_map[1] = {false}; // Initialize the single switch to OFF
+
+bool dip_switch_update_user(uint8_t index, bool active) {
+    if (index == 0) {
+        // Dip switch 1 actions
+        if (active) {
+            // Dip switch 1 is ON (Layer 3)
+            layer_on(PROFILE_2);
+            // Turn off LED2 (C5) when dip switch is pressed
+            writePinLow(C5);
+            dip_switch_map[0] = true; // Update dip switch state
+        } else {
+            // Dip switch 1 is OFF (Layer 1), activate PROFILE_1
+            layer_off(PROFILE_2);
+            layer_on(PROFILE_1);
+            // Turn on both LEDs
+            writePinHigh(C5);
+            dip_switch_map[0] = false; // Update dip switch state
+        }
+    }
+    // Add more cases for other dip switches if needed
+
+    // Default case (no need to handle for a single switch)
+    return true;
+}
---
+++ QMK/trackzeron.c
@@ -0,0 +1,207 @@
+/* Copyright 2020 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
+ * Copyright 2019 Sunjun Kim
+ * Copyright 2020 Ploopy Corporation
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "trackzeron.h"
+#include "mousekey.h"
+#include <stdint.h>
+
+#ifndef PLOOPY_DPI_OPTIONS
+#    define PLOOPY_DPI_OPTIONS { 1200, 1600, 2400 }
+#    ifndef PLOOPY_DPI_DEFAULT
+#        define PLOOPY_DPI_DEFAULT 1
+#    endif
+#endif
+#ifndef PLOOPY_DPI_DEFAULT
+#    define PLOOPY_DPI_DEFAULT 0
+#endif
+
+keyboard_config_t keyboard_config;
+uint16_t dpi_array[] = PLOOPY_DPI_OPTIONS;
+#define DPI_OPTION_SIZE (sizeof(dpi_array) / sizeof(uint16_t))
+
+// TODO: Implement libinput profiles
+// https://wayland.freedesktop.org/libinput/doc/latest/pointer-acceleration.html
+// Compile time accel selection
+// Valid options are ACC_NONE, ACC_LINEAR, ACC_CUSTOM, ACC_QUADRATIC
+
+// Trackball State
+bool     is_scroll_clicked = false;
+bool     BurstState        = false;  // init burst state for Trackball module
+uint16_t MotionStart       = 0;      // Timer for accel, 0 is resting state
+
+ __attribute__((weak)) void process_mouse_user(report_mouse_t* mouse_report, int16_t x, int16_t y) {
+    mouse_report->x = x;
+    mouse_report->y = y;
+}
+
+__attribute__((weak)) void process_mouse(report_mouse_t* mouse_report) {
+    report_pmw_t data = pmw_read_burst();
+    if (data.isOnSurface && data.isMotion) {
+        // Reset timer if stopped moving
+        if (!data.isMotion) {
+            if (MotionStart != 0) MotionStart = 0;
+            return;
+        }
+
+        // Set timer if new motion
+        if ((MotionStart == 0) && data.isMotion) {
+            if (debug_mouse) dprintf("Starting motion.\n");
+            MotionStart = timer_read();
+        }
+
+        if (debug_mouse) {
+            dprintf("Delt] d: %d t: %u\n", abs(data.dx) + abs(data.dy), MotionStart);
+        }
+        if (debug_mouse) {
+            dprintf("Pre ] X: %d, Y: %d\n", data.dx, data.dy);
+        }
+#if defined(PROFILE_LINEAR)
+        float scale = float(timer_elaspsed(MotionStart)) / 1000.0;
+        data.dx *= scale;
+        data.dy *= scale;
+#elif defined(PROFILE_INVERSE)
+        // TODO
+#else
+        // no post processing
+#endif
+        // apply multiplier
+        // data.dx *= mouse_multiplier;
+        // data.dy *= mouse_multiplier;
+
+        // Wrap to HID size
+        data.dx = constrain(data.dx, -127, 127);
+        data.dy = constrain(data.dy, -127, 127);
+        if (debug_mouse) dprintf("Cons] X: %d, Y: %d\n", data.dx, data.dy);
+        // dprintf("Elapsed:%u, X: %f Y: %\n", i, pgm_read_byte(firmware_data+i));
+
+        process_mouse_user(mouse_report, data.dx, -data.dy);
+    }
+}
+
+bool process_record_kb(uint16_t keycode, keyrecord_t* record) {
+    if (true) {
+        xprintf("KL: kc: %u, col: %u, row: %u, pressed: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed);
+    }
+
+        if (!process_record_user(keycode, record)) { return false; }
+
+    if (keycode == DPI_CONFIG && record->event.pressed) {
+        keyboard_config.dpi_config = (keyboard_config.dpi_config + 1) % DPI_OPTION_SIZE;
+        eeconfig_update_kb(keyboard_config.raw);
+        pmw_set_cpi(dpi_array[keyboard_config.dpi_config]);
+    }
+
+/* If Mousekeys is disabled, then use handle the mouse button
+ * keycodes.  This makes things simpler, and allows usage of
+ * the keycodes in a consistent manner.  But only do this if
+ * Mousekeys is not enable, so it's not handled twice.
+ */
+#ifndef MOUSEKEY_ENABLE
+    if (IS_MOUSEKEY_BUTTON(keycode)) {
+        report_mouse_t currentReport = pointing_device_get_report();
+        if (record->event.pressed) {
+            currentReport.buttons |= 1 << (keycode - KC_MS_BTN1);
+        } else {
+            currentReport.buttons &= ~(1 << (keycode - KC_MS_BTN1));
+        }
+        pointing_device_set_report(currentReport);
+        pointing_device_send();
+    }
+#endif
+
+    return true;
+}
+
+// Hardware Setup
+void keyboard_pre_init_kb(void) {
+    // debug_enable  = true;
+    // debug_matrix  = true;
+    // debug_mouse   = true;
+    // debug_encoder = true;
+
+
+    /* Ground all output pins connected to ground. This provides additional
+     * pathways to ground. If you're messing with this, know this: driving ANY
+     * of these pins high will cause a short. On the MCU. Ka-blooey.
+     */
+#ifdef UNUSED_PINS
+    const pin_t unused_pins[] = UNUSED_PINS;
+
+    for (uint8_t i = 0; i < (sizeof(unused_pins) / sizeof(pin_t)); i++) {
+        setPinOutput(unused_pins[i]);
+        writePinLow(unused_pins[i]);
+    }
+#endif
+
+    // This is the debug LED.
+#if defined(DEBUG_LED_PIN)
+    setPinOutput(DEBUG_LED_PIN);
+    writePin(DEBUG_LED_PIN, debug_enable);
+#endif
+
+    keyboard_pre_init_user();
+}
+
+void pointing_device_init(void) {
+    // initialize ball sensor
+    pmw_spi_init();
+}
+
+bool has_report_changed (report_mouse_t first, report_mouse_t second) {
+    return !(
+        (!first.buttons && first.buttons == second.buttons) &&
+        (!first.x && first.x == second.x) &&
+        (!first.y && first.y == second.y));
+}
+
+bool pointing_device_task(void) {
+    report_mouse_t mouse_report = pointing_device_get_report();
+    process_mouse(&mouse_report);
+
+    pointing_device_set_report(mouse_report);
+
+    // Check if the mouse report has changed
+    if (has_report_changed(mouse_report, pointing_device_get_report())) {
+        pointing_device_send();
+    }
+
+    // Additional processing...
+
+    // Return a boolean value indicating success or failure
+    return true; // or false, depending on your logic
+}
+
+void eeconfig_init_kb(void) {
+    keyboard_config.dpi_config = PLOOPY_DPI_DEFAULT;
+    eeconfig_update_kb(keyboard_config.raw);
+}
+
+void matrix_init_kb(void) {
+    // is safe to just read DPI setting since matrix init
+    // comes before pointing device init.
+    keyboard_config.raw = eeconfig_read_kb();
+    if (keyboard_config.dpi_config > DPI_OPTION_SIZE) {
+        eeconfig_init_kb();
+    }
+    matrix_init_user();
+}
+
+void keyboard_post_init_kb(void) {
+    pmw_set_cpi(dpi_array[keyboard_config.dpi_config]);
+
+    keyboard_post_init_user();
+}
