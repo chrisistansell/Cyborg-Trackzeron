9ebbe5121fe10de492378984f60c90e172583d6f
Chris <74263400+chrisistansell@users.noreply.github.com>
2024-03-05 12:47:37 -0500

    Modified Keymaps to show visual reperesentaton
    This will help to match keys to locations on the controller.  Flows naturally from top to bottom of the controller.

--- QMK/keymaps/default/keymap.c
+++ QMK/keymaps/default/keymap.c
@@ -2,6 +2,7 @@
  * Copyright 2019 Sunjun Kim
  * Copyright 2020 Ploopy Corporation
  * Copyright 2020 MK
+ * Copyright 2023 Coaleyed
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -23,6 +24,19 @@
 // Suspend Settings
 bool suspended = false; // Keep track of the system's suspend state
 
+     // Pinout Map - Teensy 2.0++
+     // ,------------------------------------------------------------------------------------.
+     // |       |     | C0/13 |Index|  D3/6  |Middle| F5/38 |Ring| E6/30 |Pinky|             |       Profile    LED
+     // |------------------ -----------------------------------------------------------------|     ,-------------------.
+     // |       |     | E1/12 |Index|  D2/5  |Middle| F4/37 |Ring| E7/29 |Pinky|             |     |  F6/39  | C5/C18  |  
+     // |------------------------------------------------------------------------------------|     |-------------------|
+     // |       |     | E0/11 |Index|  D1/4  |Middle| F3/36 |Ring| B4/24 |Pinky|             |     |  F7/40  | C6/C19  |
+     // |------------------------------------------------------------------------------------|     `-------------------'
+     // | C1/14 |Index| D5/8  |Index|  D0/3  |Middle| F2/35 |Ring| B5/23 |Pinky| F0/33 |Pinky|  
+     // |------------------------------------------------------------------------------------|
+     // | C2/15 |Thumb| D4/7  |Index|  B7/2  |Middle| F1/34 |Ring| B6/22 |Pinky|             |
+     // `------------------------------------------------------------------------------------'
+
 // Layer names
 #define PROFILE_1             0
 #define SECOND_LAYER_PROFILE_1 1
@@ -31,43 +45,35 @@ bool suspended = false; // Keep track of the system's suspend state
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {         
     [PROFILE_1] = {
-        //          index    middle    ring       pinky         
         {_______, KC_WH_U, KC_PGUP, KC_EQL,  KC_PAST, _______, _______},
         {_______, KC_WH_D, KC_PGDN, KC_MINS, KC_PSLS, _______, _______},
         {_______, KC_BTN1, KC_BTN2, KC_BTN3, KC_BTN4, _______, _______},
         {KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_4, KC_KP_5, KC_KP_6, _______},
         {KC_TAB,  KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_0, _______, MO(SECOND_LAYER_PROFILE_1)}
-        //^ Pinky, Index, Thumb
     },
 
     [SECOND_LAYER_PROFILE_1] = {
-        // Define the keymap for the second layer of profile 1 here
         {_______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , _______, _______},
         {_______, KC_F5  , KC_F6  , KC_F7  , KC_F8  , _______, _______},
         {_______, KC_BTN1, KC_BTN2, KC_BTN3, KC_BTN4, _______, _______},
         {_______, _______, _______, _______, _______, _______, _______},
         {_______, _______, _______, _______, _______, _______, _______}
-        //^ Pinky, Index, Thumb
     },
 
     [PROFILE_2] = {
-        //          index    middle    ring       pinky         
         {_______, KC_HOME, KC_PGUP, KC_EQL,  KC_PAST, _______, _______},
         {_______, KC_END , KC_PGDN, KC_MINS, KC_PSLS, _______, _______},
         {_______, KC_BTN1, KC_BTN2, KC_BTN3, KC_BTN4, _______, _______},
         {KC_KP_6, KC_KP_5, KC_KP_4, KC_KP_3, KC_KP_2, KC_KP_1, _______},
         {KC_TAB,  KC_KP_0, KC_KP_9, KC_KP_8, KC_KP_7, _______, MO(SECOND_LAYER_PROFILE_2)}
-        //^ Pinky, Index, Thumb
     },
 
     [SECOND_LAYER_PROFILE_2] = {
-        // Define the keymap for the second layer of profile 2 here
         {_______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , _______, _______},
         {_______, KC_F5  , KC_F6  , KC_F7  , KC_F8  , _______, _______},
         {_______, KC_BTN1, KC_BTN2, KC_BTN3, KC_BTN4, _______, _______},
         {_______, _______, _______, _______, _______, _______, _______},
         {_______, _______, _______, _______, _______, _______, _______}
-        //^ You can customize this keymap for the second layer of profile 2
     },
 };
 
