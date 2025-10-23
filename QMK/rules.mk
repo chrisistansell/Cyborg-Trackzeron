0b2115e43c213f128c248b4cf32d97844b7877a7
Chris <74263400+chrisistansell@users.noreply.github.com>
2025-10-22 23:21:34 -0400

    Update rules.mk
    Removed dip switch from code.

--- QMK/rules.mk
+++ QMK/rules.mk
@@ -6,12 +6,13 @@ F_CPU = 16000000
 
 # Bootloader selection
 BOOTLOADER = halfkay
+BOOTLOADER_SIZE = 4096
 
 # Build Options
-DIP_SWITCH_ENABLE = yes     # Virtual DIP switch configuration
 POINTING_DEVICE_ENABLE = yes
 POINTING_DEVICE_DRIVER = pmw3360
 MOUSEKEY_ENABLE = yes
+
 RGBLIGHT_ENABLE = no
 BACKLIGHT_ENABLE = no
 AUDIO_ENABLE = no
