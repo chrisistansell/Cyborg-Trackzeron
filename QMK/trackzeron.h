b7c9e0bd2a32d4b1c43be7765a0a81301da35056
Chris <74263400+chrisistansell@users.noreply.github.com>
2025-10-22 23:25:26 -0400

    Update trackzeron.h
    Removed code that wasn't needed.  Added room for EEPROM to hold last state of hardware.  If you power is lost it will return back in profile 2 versus resetting back to profile 1.

--- QMK/trackzeron.h
+++ QMK/trackzeron.h
@@ -1,27 +1,15 @@
-/* Copyright 2023 Coaleyed */
-
 #pragma once
 
 #include "quantum.h"
-#include "analog.h"
 
-// Layer definitions
-#define PROFILE_1               0
-#define SECOND_LAYER_PROFILE_1  1
-#define PROFILE_2               2
-#define SECOND_LAYER_PROFILE_2  3
+#define PROFILE_1 0
+#define PROFILE_2 1
 
-// Pin mappings
-#define DIP_SWITCH_LED_PIN F6  // LED 1
-#define PROFILE_LED_PIN C5     // LED 2
-#define DIP_SWITCH_1_PIN F7
-#define DIP_SWITCH_COUNT 1
+#define LED_1 C5
+#define PROFILE_EEPROM_ADDR 0
 
-// Function prototypes
-void update_leds_based_on_layer(void);
-void print_sensor_status(void);
-void toggle_gesture_support(void);
-void keyboard_pre_init_user(void);
+extern uint8_t active_profile;
 
-// Global flags
-extern bool gesture_enabled;
+void update_leds_from_profile(void);
+void update_profile_from_switch(void);
+
