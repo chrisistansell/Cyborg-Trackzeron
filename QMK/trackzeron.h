a9ad427ff59ac1bcd1bdfd3630091b5c08cdcea0
Chris <chrisistansell@gmail.com>
2025-11-11 00:54:33 -0500

    Defined pin C6
    Added always on code for C6

--- QMK/trackzeron.h
+++ QMK/trackzeron.h
@@ -2,18 +2,14 @@
 
 #include "quantum.h"
 
-// Profile definitions
 #define PROFILE_1 0
 #define PROFILE_2 1
 
-// LED pin assignment
-#define LED_1 C5
+#define LED_1 C6  // Always ON (active-high)
+#define LED_2 C5  // Profile indicator (active-low)
 
-// EEPROM address for storing active profile
 #define PROFILE_EEPROM_ADDR 0
 
-// Externally accessible profile state
 extern uint8_t active_profile;
 
-// Function prototypes
 void update_leds_from_profile(void);
